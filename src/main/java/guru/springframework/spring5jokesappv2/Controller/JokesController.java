44e0ed7f2500c29d00f4c030050ad51a961f46aa
Robb Turner <Turner_Robert@bah.com>
2023-07-01 08:36:07 -0500

    Added Spring MVC Controller. Closes #4.

---
+++ src/main/java/guru/springframework/spring5jokesappv2/Controller/JokesController.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring5jokesappv2.Controller;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
